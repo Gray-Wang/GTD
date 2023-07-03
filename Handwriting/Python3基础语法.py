21c6b434d444acec39df47ae84d9e794e1fe1185
1175674935 <15890772752@163.com>
2023-07-03 17:19:40 +0800

    增加手记目录 ，记录一下 python的基础操作

--- Handwriting/Python3基础语法.py
+++ Handwriting/Python3基础语法.py
@@ -123,3 +123,30 @@ class Study(unittest.TestCase):
     def test_input(self):
         inp = input("\n\n按下 enter 键后退出。")
         print(f"inp = {inp}")
+    """
+    同一行显示多条语句
+    Python 可以在同一行中使用多条语句，语句之间使用分号 ; 分割，以下是一个简单的实例：
+    """
+    def test_same_line_multiple_statements(self):
+        import sys;x = 'runoob';sys.stdout.write(x + '\n')
+
+    """
+    print 默认输出是换行的，如果要实现不换行需要在变量末尾加上 end=""：
+    """
+    def test_print(self):
+        print("111", end=" ")
+        print("1222")
+
+    """
+    import 与 from...import
+    在 python 用 import 或者 from...import 来导入相应的模块。
+    将整个模块(somemodule)导入，格式为： import somemodule
+    从某个模块中导入某个函数,格式为： from somemodule import somefunction
+    从某个模块中导入多个函数,格式为： from somemodule import firstfunc, secondfunc, thirdfunc
+    将某个模块中的全部函数导入，格式为： from somemodule import *
+    """
+    def test_import(self):
+        from sys import argv, path  # 导入特定的成员
+        print('================python from import===================================')
+        print('path:', path)  # 因为已经导入path成员，所以此处引用时不需要加sys.path
+
